b967a7948d806bf17a97ff4ae5aced2c0b783965
SirPopper <48968500+SirPopper@users.noreply.github.com>
2022-11-23 11:08:09 +0900

    Created game to play rock paper scissor
    Needs to be run in the browser. Still lots of errors in the console. Thus the result is printed as alert() and not as console.log()

--- game.js
+++ game.js
@@ -1,1 +1,34 @@
-console.log("Hello");
+let getRandomNumber = () => Math.floor(Math.random() * 3);
+
+let getComputerChoice = () => {
+  const number = getRandomNumber();
+  if (number == 0) {
+    return "rock";
+  } else if (number == 1) {
+    return "paper";
+  } else {
+    return "scissor";
+  }
+};
+
+let playersChoice = prompt("What's your choice?");
+let playerSelection = playersChoice.toLowerCase();
+
+let computerSelection = getComputerChoice();
+
+let playRound = (playerSelection, computerSelection) => {
+  if (playerSelection === computerSelection) {
+    return "even";
+  } else if (playerSelection == "scissor" && computerSelection == "paper") {
+    return "you won!";
+  } else if (playerSelection == "rock" && computerSelection == "scissor") {
+    return "you won!";
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    return "you won!";
+  } else {
+    return "you lost!";
+  }
+};
+
+alert(computerSelection);
+alert(playRound(playerSelection, computerSelection));
