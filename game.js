0508ca16baff0f05c604c139f014f28944926970
SirPopper <48968500+SirPopper@users.noreply.github.com>
2022-11-27 18:29:51 +0900

    Created buttons via DOMs and shifted logic
    Craeted buttons via DOMs with game that works. However, code is very messy, text is getting inserted above the buttons and the eventListener is too big. Need to refacor.

--- game.js
+++ game.js
@@ -25,24 +25,66 @@ let playRound = (playerSelection, computerSelection) => {
   }
 };
 
-let game = () => {
-  let count = 0;
-  for (let i = 0; i < 5; i++) {
-    let playersChoice = prompt("What's your choice?");
-    let playerSelection = playersChoice.toLowerCase();
+//adding buttons to the body through DOM
+
+//create buttons
+const btnScissor = document.createElement("button");
+btnScissor.setAttribute("id", "btn-scissor");
+btnScissor.textContent = "Scissor";
+document.body.appendChild(btnScissor);
+
+const btnRock = document.createElement("button");
+btnRock.setAttribute("id", "btn-rock");
+btnRock.textContent = "Rock";
+document.body.appendChild(btnRock);
+
+const btnPaper = document.createElement("button");
+btnPaper.setAttribute("id", "btn-paper");
+btnPaper.textContent = "Paper";
+document.body.appendChild(btnPaper);
+
+//container div
+const container = document.querySelector("#container");
+
+//eventListener
+let count = 0;
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    playerSelection = button.textContent.toLowerCase();
+    console.log(playerSelection);
 
     let computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
 
-    let result = playRound(playerSelection, computerSelection);
+    //create div to show what you chose
+    //create div to show what computer selected
+    const playerChoice = document.createElement("div");
+    playerChoice.setAttribute("id", "playerChoice");
+    playerChoice.textContent = `You chose: ${playerSelection}`;
+    container.appendChild(playerChoice);
 
-    alert(computerSelection);
-    alert(playRound(playerSelection, computerSelection));
+    //create div to show what computer selected
+    const compSelection = document.createElement("div");
+    compSelection.setAttribute("id", "compSelection");
+    compSelection.textContent = `Computer chose: ${computerSelection}`;
+    container.appendChild(compSelection);
 
-    if (result == "you won!") {
+    //create div to show results
+    const result = document.createElement("div");
+    result.setAttribute("id", "result");
+    result.textContent = `${playRound(playerSelection, computerSelection)}`;
+    container.appendChild(result);
+
+    //print count
+    if (result.textContent == "you won!") {
       count++;
+      console.log(count);
     }
-  }
-  alert("you won " + count + " times!");
-};
 
-game();
+    if (count == 5) {
+      console.log("game over");
+      count = 0;
+    }
+  });
+});
